b7e3f5c209dea99e4bcebc1b95a76cb036393d23
netzen86 <netzen0su@gmail.com>
2022-12-21 00:05:33 +0500

    Добавил обработку исключений аутентификация и таймаум коннекта

--- send_conf.py
+++ send_conf.py
@@ -1,37 +1,44 @@
 import logging
-from datetime import datetime
-from netmiko import ConnectHandler
-from file_csv import get_cred
 from concurrent.futures import ThreadPoolExecutor as Tpe
+from datetime import datetime
 from itertools import repeat
 
+from netmiko import (ConnectHandler, NetmikoAuthenticationException,
+                     NetmikoTimeoutException)
+
+from file_csv import get_cred
 
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 
 logging.basicConfig(
-    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    format="%(threadName)s %(name)s %(levelname)s: %(message)s",
     level=logging.INFO
 )
 
 
 def conf_device(devices, commands):
-    '''Function for send command on network device'''
-    start_msg = '===> {} Connection: {}'
-    received_msg = '<=== {} Received:   {}'
-    logging.info(start_msg.format(datetime.now().time(), devices['ip']))
-    with ConnectHandler(**devices) as net_connect:
-        send_cmd = net_connect.send_config_set(
-            commands[devices['device_type']]
-        )
-        save_conf = net_connect.save_config()
-        logging.info(received_msg.format(datetime.now().time(), devices['ip']))
-        return (
-            f"{'*'* 20}{devices['ip']}{'*'* 20}\n\n{send_cmd}\n{save_conf}\n\n"
-        )
+    """Function for send command on network device"""
+    start_msg = "===> {} Connection: {}"
+    received_msg = "<=== {} Received:   {}"
+    logging.info(start_msg.format(datetime.now().time(), devices["ip"]))
+    try:
+        with ConnectHandler(**devices) as net_connect:
+            send_cmd = net_connect.send_config_set(
+                commands[devices["device_type"]]
+            )
+            save_conf = net_connect.save_config()
+            logging.info(received_msg.format(
+                datetime.now().time(), devices["ip"])
+            )
+            return f'''{"*"* 20} {devices["ip"]} {"*"* 20}\n\n
+                   {send_cmd}\n{save_conf}\n\n'''
+    except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
+        return f'''{"*"* 20} {devices["ip"]} {"*"* 20}\n\n
+               !!! ERROR !!!\n\n{error}'''
 
 
 def run_parallel_session(devices, filename, commands, limit=3):
-    '''Function for run many connection to network devices'''
+    """Function for run many connection to network devices"""
     with Tpe(max_workers=limit) as executor:
         result = executor.map(conf_device, devices, repeat(commands))
     with open(filename, "w") as f:
@@ -40,14 +47,11 @@ def run_parallel_session(devices, filename, commands, limit=3):
 
 if __name__ == "__main__":
     config = {
-        'huawei': [
-            'info-center loghost 172.16.155.99 level informational',
-            'undo info-center loghost 172.16.155.88',
-            'commit'
+        "huawei": [
+            "info-center loghost 172.16.155.99 level informational",
+            "undo info-center loghost 172.16.155.88",
+            "commit",
         ],
-        'cisco_ios': [
-            'no logging 172.16.144.55',
-            'logging 172.16.144.66'
-        ]
+        "cisco_ios": ["no logging 172.16.144.55", "logging 172.16.144.66"],
     }
-    run_parallel_session(get_cred('device.csv'), "output_cmd.txt", config)
+    run_parallel_session(get_cred("device.csv"), "output_cmd.txt", config)
