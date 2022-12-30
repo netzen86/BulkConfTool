a409ddac1ccb02e72b38e6711f45621c06977e76
netzen86 <netzen0su@gmail.com>
2022-12-31 02:28:27 +0500

    намтроил аутентификацию через LDAP

--- networkmgmt/networkmgmt/settings.py
+++ networkmgmt/networkmgmt/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
+import ldap
+from django_auth_ldap.config import LDAPSearch, PosixGroupType
 
 load_dotenv()
 
@@ -78,6 +80,58 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "users.CustomUser"
+
+# Auth
+
+AUTH_LDAP_SERVER_URI = 'ldap://localhost'
+AUTH_LDAP_BIND_DN = 'cn=admin,dc=netzen,dc=dev'
+AUTH_LDAP_BIND_PASSWORD = 'StrongAdminPassw0rd'
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    'dc=netzen,dc=dev',
+    ldap.SCOPE_SUBTREE,
+    '(uid=%(user)s)'
+)
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+    'dc=example,dc=com',
+    ldap.SCOPE_SUBTREE,
+    '(objectClass=top)'
+)
+AUTH_LDAP_GROUP_TYPE = PosixGroupType(name_attr="cn")
+AUTH_LDAP_MIRROR_GROUPS = True
+
+# Populate the Django user from the LDAP directory.
+AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=groups,dc=netzen,dc=dev"
+
+AUTH_LDAP_USER_ATTR_MAP = {
+        "first_name": "givenName",
+        "last_name": "sn",
+        "email": "mail",
+        "username": "uid",
+        "password": "userPassword",
+}
+AUTH_LDAP_PROFILE_ATTR_MAP = {
+        "home_directory": "homeDirectory"
+}
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+        "is_active": "cn=active,ou=groups,dc=netzen,dc=dev",
+        "is_staff": "cn=staff,ou=groups,dc=netzen,dc=dev",
+        "is_superuser": "cn=superuser,ou=groups,dc=netzen,dc=dev"
+}
+
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+AUTH_LDAP_FIND_GROUP_PERMS = True
+AUTH_LDAP_CACHE_TIMEOUT = 3600
+
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
+# Keep ModelBackend around for per-user permissions and maybe a local
+# superuser.
+
+AUTHENTICATION_BACKENDS = (
+        'django_auth_ldap.backend.LDAPBackend',
+        'django.contrib.auth.backends.ModelBackend',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
