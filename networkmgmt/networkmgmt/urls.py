4bb6d0ecf7b7432d7fd49354b0438d7aafa0a6f1
netzen86 <netzen0su@gmail.com>
2022-12-31 02:29:04 +0500

    удалил все адреса кроме адреса админки

--- networkmgmt/networkmgmt/urls.py
+++ networkmgmt/networkmgmt/urls.py
@@ -6,9 +6,5 @@ handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
-    path('auth/', include('users.urls', namespace='users')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('about/', include('about.urls', namespace='about')),
 ]
