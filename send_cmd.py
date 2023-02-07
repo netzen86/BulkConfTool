8f8ce32994490520497338fb1813344c4ad3abdd
netzen86 <netzen0su@gmail.com>
2023-02-08 01:03:12 +0500

    try send command to network device

--- send_cmd.py
+++ send_cmd.py
@@ -1,15 +1,19 @@
 import os
+import sys
 import logging
 from concurrent.futures import ThreadPoolExecutor as Tpe
 from datetime import datetime
 from itertools import repeat
+from file_csv import get_cred
+
+
+sys.path.append('/home/netzen/Study/netmiko/')
 
 from netmiko import (ConnectHandler, NetmikoAuthenticationException,
                      NetmikoTimeoutException)
 
-from file_csv import get_cred
 
-logging.getLogger("paramiko").setLevel(logging.WARNING)
+logging.getLogger("paramiko").setLevel(logging.INFO)
 
 logging.basicConfig(
     format="%(threadName)s %(name)s %(levelname)s: %(message)s",
@@ -19,9 +23,11 @@ logging.basicConfig(
 PASSWORD = os.getenv('password')
 ENABLE = os.getenv('en_pass')
 
+
 def send_cmd(devices, commands, cmd_type):
     """Function for send command on network device"""
     cmd_show = []
+    send_cmd = ''
     model = devices['model']
     del devices['model']
     start_msg = "===> {} Connection: {}"
@@ -30,17 +36,20 @@ def send_cmd(devices, commands, cmd_type):
     try:
         with ConnectHandler(**devices) as net_connect:
             if cmd_type == 'config':
+                net_connect.config_mode()
                 for cmd in commands[model]:
-                    send_cmd = net_connect.send_command(cmd)
-                    if type(cmd) == 'list':
-                        send_cmd = net_connect.send_multiline(cmd)
+                    logging.info(f'command {type(cmd)}')
+                    if type(cmd) == list:
+                        cmd_show.append(net_connect.send_multiline(cmd, strip_command=False))
+                    cmd_show.append(net_connect.send_command(cmd, expect_string=r'\[.*?\]', strip_command=False))
+                    logging.info(f'output command {cmd_show}')
                 if model == 'nem6':
                     net_connect.commit()
-                    net_connect.save_config()
+                net_connect.save_config()
             if cmd_type == 'show':
                 for command in commands[model]:
                     cmd_show.append(net_connect.send_command(command))
-                send_cmd = " ".join(cmd_show)
+            send_cmd = " ".join(cmd_show)
             logging.info(received_msg.format(
                 datetime.now().time(), devices["ip"])
             )
@@ -69,13 +78,14 @@ if __name__ == "__main__":
             'info-center loghost 172.16.155.200 facility local6',
             'undo info-center loghost 172.16.155.101',
         ],
-        'ar6120': [
-            [['super password level 15 cipher', r'Enter Password(<8-16>):'],
-             [ENABLE, r'Confirm password:'],
-             [ENABLE, '']],
-            f'local-user test_user password irreversible-cipher {PASSWORD}',
-            'local-user test_user service-type ssh',
-            [['local-user test_user privilege level 15', r'Warning: This operation may affect online users, are you sure to change the user privilege level ?[Y/N]'],
+        'ar6120': ['sysname ar1000v',
+            [['super password level 15 cipher', r'Enter Password\(<8-16>\):'],
+             [f'{ENABLE}\n', r'Confirm password:'],
+             [f'{ENABLE}\n', '']],
+            'aaa',
+            f'local-user test_user777 password irreversible-cipher {PASSWORD}',
+            'local-user test_user777 service-type ssh',
+            [['local-user test_user777 privilege level 15', r'Warning: This operation may affect online users, are you sure to change the user privilege level ?[Y/N]'],
              ['y', '']]],
         '2901': ['no logging 172.16.144.77', 'logging 172.16.144.200'],
     }
@@ -93,5 +103,6 @@ if __name__ == "__main__":
     run_parallel_session(
         get_cred('device.csv'),
         'output_cmd.txt',
-        config_conf
+        config_conf,
+        cmd_type='config',
     )
