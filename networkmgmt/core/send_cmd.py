0cf249e88fd3afc11a93301f6688cb8191923110
netzen86 <netzen0su@gmail.com>
2023-04-03 23:45:59 +0500

    change path to netmiko and add paramiko ssh exception

--- networkmgmt/core/send_cmd.py
+++ networkmgmt/core/send_cmd.py
@@ -4,8 +4,9 @@ from django.contrib import messages
 from concurrent.futures import ThreadPoolExecutor as Tpe
 from datetime import datetime
 from itertools import repeat
+from paramiko import ssh_exception
 
-sys.path.append("netmiko/")
+sys.path.append("../netmiko/")
 
 
 from netmiko import (ConnectHandler, NetmikoAuthenticationException,
@@ -24,6 +25,9 @@ logging.basicConfig(
 def send_cmd(devices, commands, request, cmd_type):
     """Function for send command on network device"""
     cmd_show = []
+    send_cmd = ""
+    model = devices["model"]
+    del devices["model"]
     start_msg = "===> {} Connection: {}"
     received_msg = "<=== {} Received:   {}"
     error_msg = "!!! {}  Error:  {}"
@@ -35,13 +39,24 @@ def send_cmd(devices, commands, request, cmd_type):
     )
     try:
         with ConnectHandler(**devices) as net_connect:
-            if cmd_type:
-                send_cmd = net_connect.send_config_set(
-                    commands[devices['device_type']]
-                )
-                if devices['device_type'] == 'huawei_vrpv8':
+            if cmd_type == "config":
+                net_connect.config_mode()
+                for cmd in commands[model]:
+                    if type(cmd) == list:
+                        cmd_show.append(
+                            net_connect.send_multiline(cmd, cmd_verify=False)
+                        )
+                    else:
+                        logging.info(f'in else {model}')
+                        cmd_show.append(
+                            net_connect.send_command(
+                                cmd, expect_string=r"\[?.*?[\]#]",
+                                strip_command=False
+                            )
+                        )
+                if model == "nem6&ce":
                     net_connect.commit()
-                    net_connect.save_config()
+                net_connect.save_config()
             if not cmd_type:
                 for command in commands[devices['device_type']]:
                     cmd_show.append(net_connect.send_command(command))
@@ -57,7 +72,8 @@ def send_cmd(devices, commands, request, cmd_type):
             return f'''{"*"* 20} {devices["ip"]} {"*"* 20}\n\n
                    {send_cmd}\n\n'''
     except (NetmikoTimeoutException,
-            NetmikoAuthenticationException) as error_exc:
+            NetmikoAuthenticationException,
+            ssh_exception.SSHException) as error_exc:
         error = f'''{"*"* 20} {devices["ip"]} {"*"* 20}\n\n
                !!! ERROR !!!\n\n{error_exc}'''
         logging.info(error_msg.format(datetime.now().time(), error))
@@ -85,33 +101,3 @@ def run_parallel_session(
             repeat(cmd_type),
         )
     return result
-
-
-if __name__ == "__main__":
-    config_conf = {
-        'huawei': [
-            'info-center loghost 172.16.155.200 facility local6',
-            'undo info-center loghost 172.16.155.101',
-        ],
-        'huawei_vrpv8': [
-            'info-center loghost 172.16.155.200 level informational',
-            'undo info-center loghost 172.16.155.101',
-        ],
-        'cisco_ios': ['no logging 172.16.144.77', 'logging 172.16.144.200'],
-    }
-    config_show = {
-        'huawei': [
-            'display cur'
-        ],
-        'huawei_vrpv8': [
-            'display cur'
-        ],
-        'cisco_ios': [
-            'show run'
-            ],
-    }
-    run_parallel_session(
-        get_cred('device.csv'),
-        'output_cmd.txt',
-        config_show
-    )
